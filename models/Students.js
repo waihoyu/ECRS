f9ef87cfda54c17da219177d672043f85dae5207
waihoyu <13701343809@qq.com>
2019-01-22 17:37:52 +0800

    update

--- models/Students.js
+++ models/Students.js
@@ -1,22 +1,30 @@
 let mongoose = require("mongoose")
 let studentSchema = new mongoose.Schema({
-    "sid":Number,
-    "name":String,
-    "sex":String,
-    "grade":Number
+    "sid"                       :Number,
+    "name"                      :String,
+    "sex"                       :String,
+    "grade"                     :Number,
+    "password"                  :String,
+    "ChangedPassword"          :{type:Boolean,default:false}
 })
 studentSchema.statics.importStudent = function(xmlfileContent){
     let tag = 0
+    let str = "ABCDEFGHIJKLNMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
     mongoose.connection.collection("students").drop(function () {
         try {
             for (let i = 0;i < 6;i++){
                 for (let j = 1;j < xmlfileContent[i].data.length;j++){
                     // console.log(xmlfileContent[i].data[j][0])
+                    let password = ""
+                    for (let i = 0; i < 6;i++ ){
+                        password += str.charAt(parseInt(str.length * Math.random()))
+                    }
                     let s = new Student({
                         "sid":   xmlfileContent[i].data[j][0],
                         "name":  xmlfileContent[i].data[j][1],
                         "sex":   xmlfileContent[i].data[j][2]||"",
-                        "grade": i + 1
+                        "grade": i + 1,
+                        "password":  password
                     })
                     s.save()
                     // console.log(tag++)
